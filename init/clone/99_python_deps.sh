717728cd060ea53fb2903e1b6606f1495b2e8207
Nathan Oostendorp <nate@oostendorp.net>
2012-01-04 22:16:24 -0500

    add pip support

--- init/clone/99_python_deps.sh
+++ init/clone/99_python_deps.sh
@@ -3,6 +3,7 @@ cd simplecv
 python setup.py install
 cd ..
 cp -r zxing-1.6/python-zxing/zxing.py cortex/lib/python2.6/
+easy_install pip
 pip install python-pymongo 
 pip install mongoengine
 pip install cherrypy
