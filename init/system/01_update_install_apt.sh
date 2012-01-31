3a4034223e3a48fa7dcb79d754aa70042ab5b16c
Nathan Oostendorp <nate@oostendorp.net>
2012-01-31 10:27:18 -0500

    mongodb-clients -> mongodb

--- init/system/01_update_install_apt.sh
+++ init/system/01_update_install_apt.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get upgrade -y
 
 #install dependent packages
-apt-get install -y git-core subversion build-essential unzip ant openjdk-6-jdk swig gfortran cmake gcc screen pkg-config libjpeg62-dev libtiff4-dev libpng12-dev libopenexr-dev libavformat-dev libswscale-dev liblapack-dev python-dev python-setuptools python-nose python-numpy python-scipy python-pygame python-imaging mongodb mongodb-clients python-pymongo libtbb-dev redis-server libevent-dev nginx libdc1394-22-dev
+apt-get install -y git-core subversion build-essential unzip ant openjdk-6-jdk swig gfortran cmake gcc screen pkg-config libjpeg62-dev libtiff4-dev libpng12-dev libopenexr-dev libavformat-dev libswscale-dev liblapack-dev python-dev python-setuptools python-nose python-numpy python-scipy python-pygame python-imaging mongodb python-pymongo libtbb-dev redis-server libevent-dev nginx libdc1394-22-dev
 
 
